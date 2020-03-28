3b8c307fb768a4cb0d52a92401504de961b8f8c2
FindHao <find@findhao.net>
2020-03-27 22:09:47 -0400

    add redunction.float 64

--- backprop/run.sh
+++ backprop/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -rf hpctoolkit-* *csv
 hpcrun -e gpu=nvidia ./backprop 65536
 hpcstruct --gpucfg yes  hpctoolkit-backprop-measurements
-hpcrun -e gpu=nvidia,sanitizer ./backprop 65536 &> log
+hpcrun -e gpu=nvidia,sanitizer@2 ./backprop 65536 &> log
+hpcstruct ./backprop
+hpcprof -S ./backprop.hpcstruct hpctoolkit-backprop-measurements/
