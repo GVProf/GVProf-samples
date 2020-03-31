18b4fcdb4014eeebe3e00d6d9147e2d818fe43d5
FindHao <find@findhao.net>
2020-03-31 13:34:28 -0400

    modify run.sh

--- hotspot/run.sh
+++ hotspot/run.sh
@@ -2,5 +2,5 @@
 hpcrun -e gpu=nvidia ./hotspot 512 2 2 ../data/temp_512 ../data/power_512 output.out
 hpcstruct --gpucfg yes  hpctoolkit-hotspot-measurements
 hpcrun -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10 -e gpu=nvidia,sanitizer ./hotspot 512 2 2 ../data/temp_512 ../data/power_512 output.out &> log 
-hpcstruct ./gaussian
-hpcprof -S ./gaussian.hpcstruct hpctoolkit-gaussian-measurements/
+hpcstruct ./hotspot
+hpcprof -S ./hotspot.hpcstruct hpctoolkit-hotspot-measurements/
--- recursiveGaussian/run.sh
+++ recursiveGaussian/run.sh
@@ -1,6 +1,6 @@
 rm -rf hpctoolkit-* *csv
 hpcrun -e gpu=nvidia ./recursiveGaussian --file=lena_10.ppm
 hpcstruct --gpucfg yes  hpctoolkit-recursiveGaussian-measurements
-hpcrun -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10   -e gpu=nvidia,sanitizer ./recursiveGaussian --file=lena_10.ppm
-hpcstruct ./recursiveGaussian &>log
+hpcrun -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10   -e gpu=nvidia,sanitizer ./recursiveGaussian --file=lena_10.ppm &>log
+hpcstruct ./recursiveGaussian 
 hpcprof -S ./recursiveGaussian.hpcstruct hpctoolkit-recursiveGaussian-measurements/
