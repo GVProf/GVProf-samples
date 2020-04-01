bbd763f1e5d2583bb673f7f15d1c0fbbd78fd5c4
Jokeren <robinho364@gmail.com>
2020-03-31 21:30:12 -0500

    Update run configures

--- dxtc/run.sh
+++ dxtc/run.sh
@@ -1,6 +1,6 @@
 rm -rf hpctoolkit-* *csv log
-hpcrun -e gpu=nvidia ./dxtc
-hpcstruct --gpucfg yes  hpctoolkit-dxtc-measurements
-hpcrun -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10   -e gpu=nvidia,sanitizer ./dxtc &>log
+hpcrun -e gpu=nvidia ./dxtc ../data
+hpcstruct --gpucfg yes hpctoolkit-dxtc-measurements
+hpcrun -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=30 -e gpu=nvidia,sanitizer ./dxtc ../data &>log
 hpcstruct ./dxtc
 hpcprof -S ./dxtc.hpcstruct hpctoolkit-dxtc-measurements/
