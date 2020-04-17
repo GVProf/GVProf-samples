829bdf71273c4d486da0f94d03edc897b5b2ff04
Jokeren <robinho364@gmail.com>
2020-04-16 21:05:38 -0400

    Adjust pathfinder and hotspot for summit

---
+++ pathfinder_opt/run.sh
@@ -0,0 +1,7 @@
+rm -rf hpctoolkit-* *csv log
+time  ./pathfinder 100000 100 20
+hpcrun -o hpctoolkit-pathfinder-measurements -e gpu=nvidia ./pathfinder 100000 100 20
+hpcstruct --gpucfg yes hpctoolkit-pathfinder-measurements
+time hpcrun -o hpctoolkit-pathfinder-measurements -ck HPCRUN_SANITIZER_DEFAULT_TYPE=INT -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10 -e gpu=nvidia,sanitizer ./pathfinder 100000 100 20 &> log 
+hpcstruct ./pathfinder
+hpcprof -S ./pathfinder.hpcstruct hpctoolkit-pathfinder-measurements/
