3169f10f3c043e0010f896ce1320eceb6cb7e8ba
Jokeren <robinho364@gmail.com>
2020-08-30 13:59:50 -0500

    RM old script

--- backprop/run_sample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf hpctoolkit-* *csv log
-hpcrun -e gpu=nvidia -o hpctoolkit-backprop-measurements ./backprop 65536
-hpcstruct --gpucfg yes hpctoolkit-backprop-measurements
-time hpcrun -o hpctoolkit-backprop-measurements -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10 -e gpu=nvidia,sanitizer@50 ./backprop 65536 &> log
-hpcstruct ./backprop
-hpcprof -S ./backprop.hpcstruct hpctoolkit-backprop-measurements/
