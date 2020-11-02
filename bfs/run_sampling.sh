d980de5595caa9220acad15522148570d48aa7e9
Jokeren <robinho364@gmail.com>
2020-11-02 14:05:42 -0600

    Finish histogram

--- bfs/run_sampling.sh
+++ bfs/run_sampling.sh
@@ -1,6 +1,6 @@
 rm -rf hpctoolkit-* *csv log
 hpcrun -e gpu=nvidia -o hpctoolkit-bfs-measurements ./bfs ../data/graph1MW_6.txt
 hpcstruct --gpucfg yes hpctoolkit-bfs-measurements
-time hpcrun -o hpctoolkit-bfs-measurements -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10 -e gpu=nvidia,sanitizer@50 ./bfs ../data/graph1MW_6.txt &> log
+time hpcrun -o hpctoolkit-bfs-measurements -ck HPCRUN_SANITIZER_MEM_VIEWS=10 -ck HPCRUN_SANITIZER_PC_VIEWS=10 -e gpu=nvidia,redundancy@50 ./bfs ../data/graph1MW_6.txt &> log
 hpcstruct ./bfs
 hpcprof -S ./bfs.hpcstruct hpctoolkit-bfs-measurements/
